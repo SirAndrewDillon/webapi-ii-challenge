7d8dbe037e75ba2bdbb0a1f22f58141965deecab
andrew dillon <majik895@gmail.com>
2019-08-07 10:09:26 -0400

    added .post to index.js

--- posts-router.js
+++ posts-router.js
@@ -1,3 +1,61 @@
 const router = require("express").Router();
 
 const Posts = require("./data/db");
+
+router.post("/", (req, res) => {
+    const body = req.body;
+
+    // Check if there is a title and contents.
+    if (!body.title || !body.contents) {
+        // If not, return a 400.
+        res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+        });
+    } else {
+        // If so, use this method and send the body.
+        Posts.insert(body)
+            .then(result => {
+                res.status(201).json(result);
+            })
+
+            .catch(() => {
+                res.status(500).json({
+                    error: "There was an error while saving the post to the database."
+                });
+            });
+    }
+});
+
+router.post("/:id/comments", (req, res) => {
+    const comment = req.body.text;
+    const id = req.params.id;
+    Posts.findById(id)
+        .then(result => {
+            if (result.length > 0) {
+                if (comment) {
+                    //   res.status(200).json(result);
+                    Posts.insertComment(comment)
+                        .then(commentResult => {
+                            res.status(200).json(commentResult);
+                        })
+                        .catch(() => {
+                            res.status(500).json({
+                                error:
+                                    "There was an error while saving the comment to the database."
+                            });
+                        });
+                } else {
+                    res
+                        .status(400)
+                        .json({ errorMessage: "Please provide text for the comment." });
+                }
+            } else {
+                res
+                    .status(404)
+                    .json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: "The post could not be retrieved." });
+        });
+});
