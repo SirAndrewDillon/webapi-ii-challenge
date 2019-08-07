1da562d6c6ccef5bd1e9103819f77a140a1fc8b1
andrew dillon <majik895@gmail.com>
2019-08-07 09:34:38 -0400

    added cors and express to index.js

--- data/seeds/02-comments.js
+++ data/seeds/02-comments.js
@@ -1,20 +1,20 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   return knex('comments').truncate()
     .then(function () {
       return knex('comments').insert([
-        {text: "Let your workings remain a mystery. Just show people the results.", post_id: 1},
-        {text: "True mastery can be gained by letting things go their own way. It can't be gained by interfering.", post_id: 1 },
-        {text: "Not-knowing is true knowledge. Presuming to know is a disease.", post_id: 2 },
-        {text: "Success or failure: which is more destructive?", post_id: 4 },
-        {text: "When two great forces oppose each other the victory will go to the one that knows how to yield.", post_id: 4 },
-        {text: "Things arise and she let's them come; things disappear and she let's them go.", post_id: 4 },
-        {text: "A good travel has no fixed plans and is not intent upon arriving. A good artist lets his intuition lead him where ever it wants.", post_id: 5 },
-        {text: "When you look for it, there is nothing to see. When you listen for it, there is nothing to hear. When you use it, it is inexhaustible.", post_id: 7 },
-        {text: "She acts without expectation, succeeds without taking credit and doesn't think she is better than anyone else.", post_id: 7 },
-        {text: "Act without doing; work without effort.", post_id: 7 },
-        {text: "Do you have the patience to wait until your mud settles and the water is clear? Can you remain unmoving until the right action arises by itself?", post_id: 8 },
-        {text: "He who tries to shine dims his own light. He who defines himself can't know who he really is. He who clings to his work will do nothing that endures. Just do your job, then let go.", post_id: 8 }
+        { text: "People say nothing is impossible, but I do nothing every day.", post_id: 1 },
+        { text: "Just cause you got the monkey off your back doesn’t mean the circus has left town.", post_id: 1 },
+        { text: "When you have bacon in your mouth, it doesn’t matter who’s president.", post_id: 2 },
+        { text: "It’s like my dad always used to say, “if people are talking about you behind your back, just fart really loud", post_id: 4 },
+        { text: "If a kid calls his grandma Mommy and his mama Pam, he's going to jail!.", post_id: 4 },
+        { text: "Women are like the police, they could have all the evidence in the world but they still want the confession.", post_id: 4 },
+        { text: "You can never make a woman happy, it's impossible. I've never met a happy woman in my life. They're always complaining about something.", post_id: 5 },
+        { text: "I mean, they don't grade fathers. But if your daughter's a stripper, you fucked up.", post_id: 7 },
+        { text: "Do you know what the good side of crack is? If you're up at the right hour, you can get a VCR for $1.50. You can furnish your whole house for $10.95.", post_id: 7 },
+        { text: "Doctors don't cure shit! They don't cure shit! The last disease doctors cured was polio, when's the last time you met someone with polio?", post_id: 7 },
+        { text: "Anything you can suck at should make you nervous.", post_id: 8 },
+        { text: "Whoever you hate will end up in your family. You don't like gays? You're gonna have a gay son. You don't like Puerto Ricans? Your daughter's gonna come home with Livin' La Vida Loca!", post_id: 8 }
       ]);
     });
 };
